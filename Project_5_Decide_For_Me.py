d18f9131d12c30e37d689a11c862822e7a131e5b
Mariana España Feitosa <mariihespana@hotmail.com>
2020-05-27 21:50:33 -0300

    Update Project_5_Decide_For_Me.py

--- Project_5_Decide_For_Me.py
+++ Project_5_Decide_For_Me.py
@@ -1,5 +1,17 @@
 # Project 5 - Decida por mim
 # Objetivo: Faça uma pergunta para o programa e ele terá que te dar uma resposta
 
+import random
+
 class DecidaPorMim:
     def __init__(self):
+        self.respostas = [
+            'Com certeza!','Não','Sim','Pode ser','Talvez'
+        ]
+    
+    def Iniciar(self):
+        input('Faça sua pergunta: ')
+        print(random.choice(self.respostas))
+
+game = DecidaPorMim()
+game.Iniciar()
